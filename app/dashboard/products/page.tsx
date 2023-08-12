dccaf0569b4a09b0b8ce1c6fdbe8209c5239891a
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2023-08-12 19:17:41 +0500

    feat: add products page

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,5 +1,38 @@
 "use client";
 
+import React from "react";
+import { Divider, List, ListItem, Typography } from "@mui/material";
+import ProductCard from "@/app/dashboard/products/product-card";
+
 export default function Products() {
-  return <></>;
+  return (
+    <>
+      <Typography
+        sx={{ mb: 1, textAlign: "center" }}
+        variant="h4"
+        component="div"
+      >
+        Products
+      </Typography>
+      <Divider />
+      <List>
+        <ListItem>
+          <ProductCard
+            name="Aspirin"
+            category="Tablets"
+            quantity={100}
+            expiry="10/2023"
+          />
+        </ListItem>
+        <ListItem>
+          <ProductCard
+            name="Aspirin"
+            category="Tablets"
+            quantity={100}
+            expiry="10/2023"
+          />
+        </ListItem>
+      </List>
+    </>
+  );
 }
