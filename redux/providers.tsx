7bbf7f0a1f91472367b25b228ecb38d12ab5ddb0
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2023-08-12 15:50:57 +0500

    refactor: re-arrange imports

--- redux/providers.tsx
+++ redux/providers.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React from "react";
-import { store } from "./store";
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <Provider store={store}>{children}</Provider>;
