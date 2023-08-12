4c9ca3643402b44d13f721dc2512cc05bdfc5bd6
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2023-08-12 19:18:37 +0500

    feat: add reducer for products to store

--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bottomNavigationReducer from "@/app/dashboard/bottom-navigation-slice";
 import categoriesReducer from "@/app/dashboard/categories/categories-slice";
+import productsReducer from "@/app/dashboard/products/products-slice";
 
 export const store = configureStore({
   reducer: {
     bottomNavigationReducer,
     categoriesReducer,
+    productsReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
