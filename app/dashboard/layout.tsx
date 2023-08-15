0cfefca533ee536f96f480067efc34af4c452ce2
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2023-08-15 15:45:17 +0500

    feat: add dialogs to layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,6 +4,10 @@ import React from "react";
 import { Box, Paper } from "@mui/material";
 import BottomNavigation from "@/app/dashboard/bottom-navigation";
 import AddButton from "@/app/dashboard/add-button";
+import AddCategoryDialog from "@/app/dashboard/categories/add-category-dialog";
+import AddProductDialog from "@/app/dashboard/products/add-product-dialog";
+import ConsumeProductDialog from "@/app/dashboard/products/consume-product-dialog";
+import EditProductDialog from "@/app/dashboard/products/edit-product-dialog";
 
 export default function DashboardLayout({
   children,
@@ -11,15 +15,21 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Box sx={{ width: "100%" }}>
-      {children}
-      <AddButton />
-      <Paper
-        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-        elevation={3}
-      >
-        <BottomNavigation />
-      </Paper>
-    </Box>
+    <>
+      <AddCategoryDialog />
+      <EditProductDialog />
+      <AddProductDialog />
+      <ConsumeProductDialog />
+      <Box sx={{ width: "100%" }}>
+        {children}
+        <AddButton />
+        <Paper
+          sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+          elevation={3}
+        >
+          <BottomNavigation />
+        </Paper>
+      </Box>
+    </>
   );
 }
