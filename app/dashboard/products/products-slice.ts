86d7d97a82cbc5b4022477237fa82e932821504b
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2023-08-15 15:43:16 +0500

    feat: add states to products page slice

--- app/dashboard/products/products-slice.ts
+++ app/dashboard/products/products-slice.ts
@@ -2,10 +2,16 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type ProductsState = {
   isConfirmDialogOpen: boolean;
+  isAddProductDialogOpen: boolean;
+  isConsumeProductDialogOpen: boolean;
+  isEditProductDialogOpen: boolean;
 };
 
 const initialState = {
   isConfirmDialogOpen: false,
+  isAddProductDialogOpen: false,
+  isConsumeProductDialogOpen: false,
+  isEditProductDialogOpen: false,
 } as ProductsState;
 
 export const products = createSlice({
@@ -16,8 +22,23 @@ export const products = createSlice({
     setIsConfirmDialogOpen: (state, action: PayloadAction<boolean>) => {
       state.isConfirmDialogOpen = action.payload;
     },
+    setIsAddProductDialogOpen: (state, action: PayloadAction<boolean>) => {
+      state.isAddProductDialogOpen = action.payload;
+    },
+    setIsConsumeProductDialogOpen: (state, action: PayloadAction<boolean>) => {
+      state.isConsumeProductDialogOpen = action.payload;
+    },
+    setIsEditProductDialogOpen: (state, action: PayloadAction<boolean>) => {
+      state.isEditProductDialogOpen = action.payload;
+    },
   },
 });
 
-export const { reset, setIsConfirmDialogOpen } = products.actions;
+export const {
+  reset,
+  setIsConfirmDialogOpen,
+  setIsAddProductDialogOpen,
+  setIsConsumeProductDialogOpen,
+  setIsEditProductDialogOpen,
+} = products.actions;
 export default products.reducer;
