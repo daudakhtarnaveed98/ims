b4a58659fd7de0f06e50e50f09ca6d3275770a88
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2023-08-18 04:39:04 +0500

    feat: add new states for data fetching and ui

--- app/dashboard/categories/categories-slice.ts
+++ app/dashboard/categories/categories-slice.ts
@@ -1,13 +1,15 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
-type CategoriesState = {
-  isConfirmDialogOpen: boolean;
-  isAddCategoryDialogOpen: boolean;
-};
+import { CategoriesState, Category } from "@/app/dashboard/categories/types";
 
 const initialState = {
   isConfirmDialogOpen: false,
   isAddCategoryDialogOpen: false,
+  isAddingCategory: false,
+  isFetchingCategory: false,
+  isDeletingCategory: false,
+  toDeleteCategoryId: "",
+  categories: [],
+  refetchCategories: false,
 } as CategoriesState;
 
 export const categories = createSlice({
@@ -21,9 +23,36 @@ export const categories = createSlice({
     setIsAddCategoryDialogOpen: (state, action: PayloadAction<boolean>) => {
       state.isAddCategoryDialogOpen = action.payload;
     },
+    setIsAddingCategory: (state, action: PayloadAction<boolean>) => {
+      state.isAddingCategory = action.payload;
+    },
+    setIsFetchingCategory: (state, action: PayloadAction<boolean>) => {
+      state.isFetchingCategory = action.payload;
+    },
+    setIsDeletingCategory: (state, action: PayloadAction<boolean>) => {
+      state.isDeletingCategory = action.payload;
+    },
+    setToDeleteCategoryId: (state, action: PayloadAction<string>) => {
+      state.toDeleteCategoryId = action.payload;
+    },
+    setCategories: (state, action: PayloadAction<Category[]>) => {
+      state.categories = action.payload;
+    },
+    setRefetchCategories: (state, action: PayloadAction<boolean>) => {
+      state.refetchCategories = action.payload;
+    },
   },
 });
 
-export const { reset, setIsConfirmDialogOpen, setIsAddCategoryDialogOpen } =
-  categories.actions;
+export const {
+  reset,
+  setIsConfirmDialogOpen,
+  setIsAddCategoryDialogOpen,
+  setIsAddingCategory,
+  setIsFetchingCategory,
+  setIsDeletingCategory,
+  setToDeleteCategoryId,
+  setCategories,
+  setRefetchCategories,
+} = categories.actions;
 export default categories.reducer;
