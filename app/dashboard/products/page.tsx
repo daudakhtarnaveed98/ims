0a6aa3dab59c21d476a72219d010a83254c756a0
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2023-08-20 01:45:31 +0500

    feat: add products dynamically

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,76 +1,69 @@
 "use client";
 
 import React from "react";
-import { Divider, List, ListItem, Typography } from "@mui/material";
+import {
+  Alert,
+  CircularProgress,
+  Divider,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
 import ProductCard from "@/app/dashboard/products/product-card";
+import { useProducts } from "@/app/dashboard/products/hooks";
+import { useAppSelector } from "@/redux/hooks";
+import Box from "@mui/material/Box";
 
 export default function Products() {
+  useProducts();
+  const products = useAppSelector((state) => state.productsReducer.products);
+  const isFetchingProducts = useAppSelector(
+    (state) => state.productsReducer.isFetchingProducts,
+  );
+
   return (
     <>
       <Typography
-        sx={{ mb: 1, textAlign: "center", position: "static" }}
+        sx={{ mb: 1, textAlign: "center" }}
         variant="h4"
         component="div"
       >
         Products
       </Typography>
       <Divider />
-      <List sx={{ mb: 8 }}>
-        <ListItem>
-          <ProductCard
-            name="Aspirin"
-            category="Tablets"
-            stock={100}
-            expiry="10/2023"
-            modifiedOn="01/10/2023"
-          />
-        </ListItem>
-        <ListItem>
-          <ProductCard
-            name="Aspirin"
-            category="Tablets"
-            stock={100}
-            expiry="10/2023"
-            modifiedOn="01/10/2023"
-          />
-        </ListItem>
-        <ListItem>
-          <ProductCard
-            name="Aspirin"
-            category="Tablets"
-            stock={100}
-            expiry="10/2023"
-            modifiedOn="01/10/2023"
-          />
-        </ListItem>
-        <ListItem>
-          <ProductCard
-            name="Aspirin"
-            category="Tablets"
-            stock={100}
-            expiry="10/2023"
-            modifiedOn="01/10/2023"
-          />
-        </ListItem>
-        <ListItem>
-          <ProductCard
-            name="Aspirin"
-            category="Tablets"
-            stock={100}
-            expiry="10/2023"
-            modifiedOn="01/10/2023"
-          />
-        </ListItem>
-        <ListItem>
-          <ProductCard
-            name="Aspirin"
-            category="Tablets"
-            stock={100}
-            expiry="10/2023"
-            modifiedOn="01/10/2023"
-          />
-        </ListItem>
-      </List>
+      {isFetchingProducts ? (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="60vh"
+        >
+          <CircularProgress />
+        </Box>
+      ) : products.length > 0 ? (
+        <List sx={{ mb: 8 }}>
+          {products.map((product) => (
+            <ListItem key={product.id}>
+              <ProductCard
+                id={product.id}
+                name={product.name}
+                category={product.category}
+                stock={product.stock}
+                expiry={product.expiry}
+                modifiedOn={product.modifiedOn}
+                lowStockQuantity={product.lowStockQuantity}
+              />
+            </ListItem>
+          ))}
+        </List>
+      ) : (
+        <Alert
+          severity="warning"
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
+          No products found.
+        </Alert>
+      )}
     </>
   );
 }
