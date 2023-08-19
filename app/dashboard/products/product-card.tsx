9e6f28af7770e4db0c2fc14fcd38b9eccf9a6d77
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2023-08-20 01:46:23 +0500

    feat: add functionality for buttons to edit and consume products

--- app/dashboard/products/product-card.tsx
+++ app/dashboard/products/product-card.tsx
@@ -8,31 +8,54 @@ import {
   setIsConfirmDialogOpen,
   setIsConsumeProductDialogOpen,
   setIsEditProductDialogOpen,
+  setToConsumeProduct,
+  setToDeleteProductId,
+  setToEditProduct,
 } from "@/app/dashboard/products/products-slice";
-import { IconButton } from "@mui/material";
+import { Chip, IconButton } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Product } from "@/app/dashboard/products/types";
+import { DateTime } from "luxon";
 
 export default function ProductCard({
+  id,
   name,
   category,
   stock,
   expiry,
   modifiedOn,
+  lowStockQuantity,
 }: Product) {
   const dispatch = useAppDispatch();
+  const expiresInMonths = DateTime.fromFormat(expiry, "MM/yyyy")
+    .diff(DateTime.now(), "months")
+    .toObject()["months"];
 
   return (
     <Card sx={{ width: "100%" }}>
+      {stock <= lowStockQuantity && (
+        <Chip
+          sx={{ m: 1, float: "right" }}
+          label="Low stock"
+          color="warning"
+          size="small"
+        />
+      )}
+      {expiresInMonths && expiresInMonths <= 6 && (
+        <Chip
+          sx={{ mt: 1, float: "right" }}
+          label="Expires soon"
+          color="error"
+          size="small"
+        />
+      )}
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           {category}
         </Typography>
-        <Typography variant="h6" component="div">
-          {name}
-        </Typography>
+        <Typography variant="h6">{name}</Typography>
         <Typography color="text.secondary">Stock: {stock}</Typography>
         <Typography color="text.secondary">Expiry: {expiry}</Typography>
         <Typography color="text.secondary">
@@ -42,19 +65,70 @@ export default function ProductCard({
       <CardActions>
         <IconButton
           color="primary"
-          onClick={() => dispatch(setIsEditProductDialogOpen(true))}
+          onClick={() => {
+            if (
+              id != null &&
+              name != null &&
+              category != null &&
+              stock != null &&
+              expiry != null &&
+              modifiedOn != null &&
+              lowStockQuantity != null
+            ) {
+              dispatch(
+                setToEditProduct({
+                  id,
+                  name,
+                  category,
+                  stock,
+                  expiry,
+                  modifiedOn,
+                  lowStockQuantity,
+                }),
+              );
+              dispatch(setIsEditProductDialogOpen(true));
+            }
+          }}
         >
           <EditIcon sx={{ height: 24, width: 24 }} />
         </IconButton>
         <IconButton
           color="warning"
-          onClick={() => dispatch(setIsConsumeProductDialogOpen(true))}
+          onClick={() => {
+            if (
+              id != null &&
+              name != null &&
+              category != null &&
+              stock != null &&
+              expiry != null &&
+              modifiedOn != null &&
+              lowStockQuantity != null
+            ) {
+              dispatch(
+                setToConsumeProduct({
+                  id,
+                  name,
+                  category,
+                  stock,
+                  expiry,
+                  modifiedOn,
+                  lowStockQuantity,
+                }),
+              );
+              dispatch(setIsConsumeProductDialogOpen(true));
+            }
+          }}
         >
           <ShoppingCartIcon sx={{ height: 24, width: 24 }} />
         </IconButton>
         <IconButton
           color="error"
-          onClick={() => dispatch(setIsConfirmDialogOpen(true))}
+          onClick={() => {
+            if (id != null) {
+              dispatch(setToDeleteProductId(id));
+              dispatch(setIsConfirmDialogOpen(true));
+            }
+          }}
         >
           <DeleteIcon sx={{ height: 24, width: 24 }} />
         </IconButton>
