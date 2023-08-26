40bbd7c690ea5cd2db8509385a12598f58cb7d24
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2023-08-26 22:46:04 +0500

    feat: remove edit button

--- app/dashboard/users/user-card.tsx
+++ app/dashboard/users/user-card.tsx
@@ -5,14 +5,11 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { useAppDispatch } from "@/redux/hooks";
 import { IconButton } from "@mui/material";
-import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { User } from "@/app/dashboard/users/types";
 import {
   setIsConfirmDialogOpen,
-  setIsEditUserDialogOpen,
   setToDeleteUser,
-  setToEditUser,
 } from "@/app/dashboard/users/users-slice";
 
 export default function UserCard({ id, email, role, uid }: User) {
@@ -32,24 +29,6 @@ export default function UserCard({ id, email, role, uid }: User) {
         <Typography color="text.secondary">Role: {role}</Typography>
       </CardContent>
       <CardActions>
-        <IconButton
-          color="primary"
-          onClick={() => {
-            if (id != null && uid != null && email != null && role != null) {
-              dispatch(
-                setToEditUser({
-                  id,
-                  uid,
-                  email,
-                  role,
-                }),
-              );
-              dispatch(setIsEditUserDialogOpen(true));
-            }
-          }}
-        >
-          <EditIcon sx={{ height: 24, width: 24 }} />
-        </IconButton>
         <IconButton
           color="error"
           onClick={() => {
