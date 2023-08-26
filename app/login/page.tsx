112dc23e39ab84334f18e47d847cf970944d4881
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2023-08-26 23:21:22 +0500

    feat: remove reset password link

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,6 @@ import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
@@ -241,13 +240,6 @@ export default function SignIn() {
             >
               Sign In
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Reset password
-                </Link>
-              </Grid>
-            </Grid>
           </Box>
         </form>
       </Box>
