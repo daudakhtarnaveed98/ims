a58f9d1eda5ae420f9f1edb2713c762042797009
Daud Akhtar Naveed <daudakhtarnaveed@gmail.com>
2023-08-26 14:26:18 +0500

    feat: add types for user and users page

---
+++ app/dashboard/users/types.ts
@@ -0,0 +1,20 @@
+export interface User {
+  id: string;
+  uid: string;
+  email: string;
+  password?: string;
+  role: string;
+}
+
+export interface UsersState {
+  isAddUserDialogOpen: boolean;
+  isConfirmDialogOpen: boolean;
+  isEditUserDialogOpen: boolean;
+  isFetchingUsers: boolean;
+  isAddingUser: boolean;
+  isDeletingUser: boolean;
+  toDeleteUser: User;
+  toEditUser: User;
+  users: User[];
+  refetchUsers: boolean;
+}
